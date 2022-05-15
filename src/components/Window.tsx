9de343aee832ca38028cee46ae0282518e9ef29d
bugra <bumengi94@gmail.com>
2022-05-15 16:46:19 +0300

    window added

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,7 +1,23 @@
 import React, { FC } from "react";
 
 const Window: FC = () => {
-	return <main>Main</main>;
+	return (
+		<main>
+			<div className="window">
+				<div className="header">
+					<button className={"control close"} />
+					<button className={"control minimize"} />
+					<button className={"control maximize"} />
+				</div>
+				<div className="content">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tempor
+						consectetur, nisi nunc interdum nisl, eget egestas nunc nisl euismod nunc.
+					</p>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Window;
